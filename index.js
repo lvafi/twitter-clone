f4a5243a3b08deedecee848e87207b0cdcaa54ca
ladan-vafi <lydia.vafi@gmail.com>
2020-12-20 23:48:36 -0800

    Add route for our app

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const { response } = require('express');
 const app = express();
+
+app.get("/welcome", (request, response) => {
+    response.send("Hello World!");
+});
 
 app.listen(3000, () => {
     console.log('Express web app on localhost: 3000');
