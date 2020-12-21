2ff4e8d3df06a048e5413ca7b89e789821c47d3d
ladan-vafi <lydia.vafi@gmail.com>
2020-12-21 07:34:57 -0800

    Modify index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const { response } = require('express');
 const app = express();
 const logger = require('morgan');
 
+app.set('view engine', 'ejs');
+
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 
 app.get("/welcome", (request, response) => {
-    response.send("Hello World!");
+    response.render('welcome');
 });
 
 app.listen(3000, () => {
